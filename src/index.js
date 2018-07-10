28e7c0714aab7fb6d47b784707db56b21a9768e8
alex <superalex_1104@163.com>
2018-07-10 21:39:39 +0800

    各种loader怎么用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/styles/test.css'
+import './assets/styles/test.styl'
+import './assets/images/IMG_20170125_083856.jpg'
+
 const root = document.createElement('div')
 document.body.appendChild(root)
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+    mode: 'development',
     entry: path.join(__dirname,'src/index.js'),
     output: {
         filename: 'bundle.js',
@@ -10,12 +11,32 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.vue$/,
+                test: /\.vue$/,
                 use: 'vue-loader'
             },
             {
-                test: /.css$/,
+                test: /\.css$/,
                 use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 102400,
+                            name: '[name]-aaa.[ext]'
+                        }
+                    }
+                ] 
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
             }
         ]
     },

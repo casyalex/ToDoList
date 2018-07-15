df8b7c19b89da5ac4193a6f16a61cd0ded90093b
xyx <superalex_1104@163.com>
2018-07-15 14:19:12 +0800

    extract css

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDEV = process.env.NODE_ENV === 'development'
 
 const config = {
     mode: 'development',
-    entry: path.join(__dirname,'src/index.js'),
+    entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname,'dist')
+        filename: 'bundle.[hash:8].js',
+        path: path.join(__dirname, 'dist')
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.vue$/,
                 use: 'vue-loader'
             },
@@ -23,34 +23,14 @@ const config = {
                 use: 'babel-loader'
             },
             {
-                test: /\.css$/,
-                use: ['style-loader','css-loader']
-            },
-            {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 102400,
-                            name: '[name]-aaa.[ext]'
-                        }
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 102400,
+                        name: '[name]-aaa.[ext]'
                     }
-                ] 
-            },
-            {
-                test: /\.styl/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
+                }]
             }
         ]
     },
@@ -66,18 +46,53 @@ const config = {
 }
 
 if (isDEV) {
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
-        port: 8000,
-        host: '0.0.0.0', //win10直接访问0.0.0.0是不行的，要访问本机ip
-        overlay: {
-            error: true
+            port: 8000,
+            host: '0.0.0.0', //win10直接访问0.0.0.0是不行的，要访问本机ip
+            overlay: {
+                error: true
+            },
+            hot: true
         },
-        hot: true
-    },
+        config.plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        )
+} else {
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
     config.plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
+        new ExtractPlugin('style.[hash:8].css')
     )
 }
 

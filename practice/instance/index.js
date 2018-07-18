4e8be95939cc53cb361f55be711cc8325975873f
xyx <superalex_1104@163.com>
2018-07-18 15:31:33 +0800

    Vue 实例

---
+++ practice/instance/index.js
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import { setInterval } from 'timers'
+
+const app = new Vue({
+  // el: '#root',
+  template: '<div ref="div">{{text}} {{obj.a}}</div>',
+  data: {
+    text: 0,
+    obj: {}
+  }
+  // watch: {
+  //   text (newText, oldText) {
+  //     console.log(`${newText} : ${oldText}`)
+  //   }
+  // }
+})
+
+app.$mount('#root')
+
+// let i = 0
+setInterval(() => {
+  // i++
+  app.text += 1
+  app.text += 1
+  app.text += 1
+  app.text += 1
+  app.text += 1
+  // app.obj.a = i
+  // app.$set(app.obj, 'a', i)
+  // app.$delete(app.obj, 'a')
+  // app.$forceUpdate()
+  // app.$options.data.text += 1  不是同一个对象，无效
+  // app.$data.text += 1
+}, 1000)
+
+// console.log(app.$data)
+// console.log(app.$props)
+// console.log(app.$el)
+// console.log(app.$options)
+
+// app.$options.render = (h) => {
+//   return h('div', {}, 'new render function')
+// }
+
+// console.log(app.$root)
+// console.log(app.$root === app)
+// console.log(app.$children)
+// console.log(app.$slots)
+// console.log(app.$scopedSlots)
+
+// console.log(app.$refs)
+// console.log(app.$isServer)
+
+// const unWatch = app.$watch('text', (newText, oldText) => {
+//   console.log(`${newText} : ${oldText}`)
+// })
+
+// setTimeout(() => {
+//   unWatch()
+// }, 2000)
+
+// app.$on('test', (a, b) => {
+//   console.log(`'test emited' ${a} ${b}`)
+// })
+// app.$emit('test', 1, 2)
+
+// app.$once('test', (a, b) => {
+//   console.log(`'test emited' ${a} ${b}`)
+// })
+
+// setInterval(() => {
+//   app.$emit('test', 1, 2)
+// }, 1000)
+
+// app.$forceUpdate()
+// app.$nextTick()
